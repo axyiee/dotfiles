3c545fb023be823f91c2e09a3473a0952cd835a0
eexsty <hello@exst.fun>
2022-03-26 15:27:49 -0300

    Add support for my Gentoo overlay

--- gentoo/packages.sh
+++ gentoo/packages.sh
@@ -9,10 +9,10 @@ fi
 emerge --verbose dev-vcs/git app-eselect/eselect-repository
 
 eselect repository enable elementary guru
+eselect repository add eexsty https://github.com/eexsty/gentoo-overlay
 emerge --sync
 
-# need to install picom, polkit dumb agent, web browser and droid nerd font manually
-# https://github.com/dccsillag/picom
+# need to install polkit dumb agent, web browser and droid nerd font manually
 #https://github.com/sandsmark/polkit-dumb-agent
 
 emerge --ask --verbose media-fonts/inter media-fonts/noto-emoji media-fonts/noto-cjk media-fonts/droid \
@@ -21,7 +21,7 @@ emerge --ask --verbose media-fonts/inter media-fonts/noto-emoji media-fonts/noto
     x11-wm/bspwm x11-misc/sxhkd x11-misc/polybar x11-misc/dunst x11-terms/alacritty x11-misc/rofi \
     app-shells/starship app-shells/zsh-autosuggestions app-shells/zsh-syntax-highlighting app-shells/fzf \
     sys-apps/dbus net-wireless/wpa_supplicant net-misc/dhcpcd sys-kernel/linux-firmware sys-apps/usb_modeswitch \
-    media-gfx/maim x11-apps/setxkbmap
+    media-gfx/maim x11-apps/setxkbmap x11-misc/picom-animations
 
 rc-update add wpa_supplicant
 rc-update add dbus
